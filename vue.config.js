16e39a23ff6980e61970934b236bc1a1d8962b33
Thom Bruce <thom@thombruce.com>
2021-03-10 13:55:59 +0000

    Init Vue in same directory

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+module.exports = {
+  // disable hashes in filenames
+  // filenameHashing: false,
+  // delete HTML related webpack plugins
+  chainWebpack: config => {
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+  }
+}
