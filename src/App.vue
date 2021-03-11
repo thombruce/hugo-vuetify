8cc1200f443663c67d3db427d098d5469a645856
Thom Bruce <thom@thombruce.com>
2021-03-11 13:48:20 +0000

    add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template lang="pug">
-  #app
-    #nav
-      router-link(to="/") Home
-      router-link(to="/about") About
-    router-view
+  VApp
+    VMain
+      RouterView
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
 // vue.config.js
 module.exports = {
   // disable hashes in filenames
-  // filenameHashing: false,
+  filenameHashing: false,
   // delete HTML related webpack plugins
   chainWebpack: config => {
     config.plugins.delete('html')
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
-  }
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
