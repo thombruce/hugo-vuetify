ff5286fe16808ba2a43e2256b1fc9d58be51de01
Thom Bruce <thom@thombruce.com>
2021-03-11 12:04:14 +0000

    remove dummy component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template lang="pug">
   .home
-    img(alt="Vue logo" src="../assets/logo.png")
-    HelloWorld(msg="Welcome to Your Vue.js App")
+    h1 This is the home page
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
