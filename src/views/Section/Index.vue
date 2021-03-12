ba12294563a277c1a371208e52df9349fe8dbb02
Thom Bruce <thom@thombruce.com>
2021-03-12 17:32:31 +0000

    add content pages

---
+++ src/views/Section/Index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .home
+    h1 This is an index page
+    h2(v-for="post in posts") {{ post.title }}
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Section',
+
+  data () {
+    return {
+      posts: null
+    }
+  },
+
+  mounted () {
+    this.axios
+      .get('/index.json')
+      .then(response => (this.posts = response.data.data))
+  }
+}
+</script>
---
+++ src/views/Section/Show.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  .home
+    h1 This is an individual page
+    h2(v-if="post") {{ post.title }}
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Post',
+
+  data () {
+    return {
+      post: null
+    }
+  },
+
+  mounted () {
+    this.axios
+      .get(`/${this.$route.params.section}/${this.$route.params.id}/index.json`)
+      .then(response => (this.post = response.data.data))
+  }
+}
+</script>
